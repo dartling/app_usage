e0164673658afee15e375fd97b10c11ba6fa0dd7
yallurium <christos@dartling.dev>
2022-03-27 20:56:48 +0200

    Create plugin from template

---
+++ test/app_usage_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:app_usage/app_usage.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('app_usage');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await AppUsage.platformVersion, '42');
+  });
+}
